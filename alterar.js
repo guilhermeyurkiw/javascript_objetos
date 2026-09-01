e5941d110b8de6959c01da20b1f9c1187ca8f50a
guilhermeyurkiw <guilherme.yurkiw@escola.pr.gov.br>
2026-09-01 15:20:52 +0000

    .

--- alterar.js
+++ alterar.js
@@ -1,4 +1,4 @@
-const pessoa = {
+let pessoa = {
   nome: "Luma",
   profissao: "Engenheira",
 };
@@ -10,3 +10,13 @@ console.log(pessoa.telefone);
 pessoa.telefone = "11 2223333444";
 
 console.log(pessoa.telefone);
+
+pessoa.nome = "Luma Silva";
+
+console.log(pessoa);
+
+const novaPessoa = {
+  nome: "Pedro",
+};
+
+pessoa = novaPessoa;
